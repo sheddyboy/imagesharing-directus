5811c073a98f5c9e351ba0ce37a15be7d8a3df5d
Sheddy <shedrackitulah@gmail.com>
2023-09-21 13:43:41 +0100

    chore: :heavy_plus_sign: add updateAllowPublicObject function

--- src/components/YesOrNo.tsx
+++ src/components/YesOrNo.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter,useParams } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 import Button from "./Button";
+import { updateAllowPublicObject } from "@/utils";
 
 interface YesOrNoProps {
   uploadUUID: string;
@@ -11,30 +12,18 @@ interface YesOrNoProps {
 export default function YesOrNo({ uploadUUID }: YesOrNoProps) {
   const [selectedBtnId, setSelectedBtnId] = useState<null | "yes" | "no">(null);
   const router = useRouter();
-  const {share_id} = useParams();
+  const { share_id } = useParams();
 
   const handleButtonClick = (btnId: "yes" | "no") => {
     if (selectedBtnId == null) {
       // Only change the state if the clicked button is not already active
       setSelectedBtnId(btnId);
-      fetch(
-        `${process.env.NEXT_PUBLIC_BACKENDURL}/items/${process.env.NEXT_PUBLIC_COLLECTION_NAME}/${uploadUUID}`,
-        {
-          method: "PATCH",
-          body: JSON.stringify({
-            allowPublic: btnId === "yes" ? true : false,
-          }),
-          cache: "no-store",
-        }
-      )
-        .then((res) =>{
-          console.log("res",res)
-          router.push(
-            `/${share_id}/download?allowPublic=${btnId === "yes" ? "true" : "false"}`
-          )
-        }
-        )
-        .catch((err) => console.log("err", err));
+      updateAllowPublicObject({
+        router,
+        share_id: share_id as string,
+        uploadUUID,
+        value: btnId === "yes" ? true : false,
+      });
     }
   };
   return (
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,48 +1,80 @@
 import { AuthWithDirectusRes, ItemsFromCollection } from "@/app/types";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 
 export async function authenticateWithDirectus({
-    share_id,
-  }: {
-    share_id: string;
-  }): Promise<AuthWithDirectusRes> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/shares/auth`, {
-      method: "POST",
+  share_id,
+}: {
+  share_id: string;
+}): Promise<AuthWithDirectusRes> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/shares/auth`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      share: share_id,
+    }),
+    cache: "no-store",
+  });
+  if (res.status !== 200) {
+    const data = await res.json();
+    throw new Error(JSON.stringify(data));
+  }
+
+  return res.json();
+}
+export async function getItemsFromCollection({
+  NEXT_PUBLIC_COLLECTION_NAME,
+  accessToken,
+}: {
+  NEXT_PUBLIC_COLLECTION_NAME: string;
+  accessToken: string;
+}): Promise<ItemsFromCollection> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKENDURL}/items/${NEXT_PUBLIC_COLLECTION_NAME}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  if (res.status !== 200) {
+    const data = await res.json();
+    throw new Error(JSON.stringify(data));
+  }
+
+  return res.json();
+}
+
+export async function updateAllowPublicObject({
+  uploadUUID,
+  share_id,
+  value,
+  router,
+}: {
+  uploadUUID: string;
+  share_id: string;
+  value: boolean;
+  router: AppRouterInstance;
+}) {
+  fetch(
+    `${process.env.NEXT_PUBLIC_BACKENDURL}/items/${process.env.NEXT_PUBLIC_COLLECTION_NAME}/${uploadUUID}`,
+    {
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        share: share_id,
+        allowPublic: value,
       }),
       cache: "no-store",
-    });
-    if (res.status !== 200) {
-      const data = await res.json();
-      throw new Error(JSON.stringify(data));
     }
-  
-    return res.json();
-  }
-  export async function getItemsFromCollection({
-    NEXT_PUBLIC_COLLECTION_NAME,
-    accessToken,
-  }: {
-    NEXT_PUBLIC_COLLECTION_NAME: string;
-    accessToken: string;
-  }): Promise<ItemsFromCollection> {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKENDURL}/items/${NEXT_PUBLIC_COLLECTION_NAME}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-  
-    if (res.status !== 200) {
-      const data = await res.json();
-      throw new Error(JSON.stringify(data));
-    }
-  
-    return res.json();
-  }
+  )
+    .then((res) => {
+      console.log("res", res);
+      router.push(`/${share_id}/download?allowPublic=${value}`);
+    })
+    .catch((err) => console.log("err", err));
+}
